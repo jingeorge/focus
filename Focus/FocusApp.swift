9fc1b45bb24bf2e46a1ca0aef6e74811fd67abe4
georgejin <george.jin@tum.de>
2024-10-19 19:57:34 +0200

    Initial Commit

---
+++ Focus/FocusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusApp.swift
+//  Focus
+//
+//  Created by George Jin on 19.10.24.
+//
+
+import SwiftUI
+
+@main
+struct FocusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FocusTests/FocusTests.swift
@@ -0,0 +1,17 @@
+//
+//  FocusTests.swift
+//  FocusTests
+//
+//  Created by George Jin on 19.10.24.
+//
+
+import Testing
+@testable import Focus
+
+struct FocusTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
